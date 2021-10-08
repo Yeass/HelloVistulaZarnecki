ae73c300e6960ee30f29fa716d482a3a8bf2e84e
student <student@lab.pl.wsei>
2021-10-08 12:03:28 +0200

    Debug_update

--- src/pl/vistula/helloworldzarnecki/HelloWorldZarnecki.java
+++ src/pl/vistula/helloworldzarnecki/HelloWorldZarnecki.java
@@ -4,5 +4,10 @@ public class HelloWorldZarnecki {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Zarnecki");
         System.out.println("Poszedlbym spanko w sumie.");
+
+        int x = 3;
+        int y = 7;
+
+        int z = x + y;
     }
 }
