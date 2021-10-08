f9e647a118efa58a0b2556ad714e94bd9474432c
student <student@lab.pl.wsei>
2021-10-08 11:48:45 +0200

    Text_update

--- src/pl/vistula/helloworldzarnecki/HelloWorldZarnecki.java
+++ src/pl/vistula/helloworldzarnecki/HelloWorldZarnecki.java
@@ -3,5 +3,6 @@ package pl.vistula.helloworldzarnecki;
 public class HelloWorldZarnecki {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Zarnecki");
+        System.out.println("Poszedlbym spanko w sumie.");
     }
 }
