f9772f47d401a1b0c6532efd4fd609ef59c4e76f
student <student@lab.pl.wsei>
2021-10-08 11:37:56 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldzarnecki/HelloWorldZarnecki.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldzarnecki;
+
+public class HelloWorldZarnecki {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Zarnecki");
+    }
+}
